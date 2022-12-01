4bf035be6f39a4892c403687e5f4128b988e5ce0
Ni-speed <railmir@gmail.com>
2022-12-01 18:29:50 +0500

    Revert "Commit4"
    This reverts commit a1e8bb65ff40660a13eee72b1a9773ab8872e9ba.

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -1,9 +1,1 @@
 import React from 'react'
-let test1 = 'test'
-let test2 = 'test'
-let test3 = 'test'
-let test4 = 'test'
-let test5 = 'test'
-
-
-
--- src/User/User.tsx
+++
@@ -1,2 +0,0 @@
-let test = 'test'
-let newTest = 10
